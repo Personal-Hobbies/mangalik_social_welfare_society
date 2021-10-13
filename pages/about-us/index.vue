1f0bb529f2ef617d5d99782c11137c8d277a1fe7
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-13 11:51:46 +0530

    added index to about-us

---
+++ pages/about-us/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="w-full">
+		<section class="text-gray-600 body-font bg-indigo-100">
+			<div class="container px-5 pt-24 pb-10 mx-auto flex flex-wrap items-center">
+				<div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+					<h1 class="title-font font-medium text-3xl text-gray-900">
+						Mangalik Social Welfare Society
+					</h1>
+					<p class="leading-relaxed mt-4 text-justify">
+						The <b>Mangalik Social Welfare Society (MSWS)</b> has completed its second formal years and marching for the third formal years of its existence for extending welfare and development services for the people of North Tripura district.  However, the Society was emerged with another name, <b>Bharatiya Health Mission (BHM)</b> but considering similar names of one central government, the same was declined and was registered as <b>Mangalik Social Welfare Society (MSWS)</b> on <b>6th April 2017</b>. Since inception, the MSWS is extending its welfare and development services for the needy poor people in one hand and also organizing its efforts for its organizational development.
+					</p>
+					<div class="flex flex-wrap w-full text-justify">
+						<p class="w-full leading-relaxed text-gray-500">
+							This year it has also taken initiatives to make its community clinic little larger and even the village panchayat Bargul has also extends its financial support for the same.
+						</p>
+					</div>
+
+				</div>
+				<div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+
+				</div>
+			</div>
+		</section>
+
+		<section class="text-gray-600 body-font py-12">
+			<div class="container px-5 mx-auto flex flex-wrap items-center">
+				<div class="flex flex-wrap w-full text-justify">
+					<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+						<div class="flex flex-wrap w-full text-justify pb-6">
+							<p class="w-full leading-relaxed text-gray-500">
+								The MSWS has adopted suitable strategies for implementation of its development interventions and to make it more transparent, the MSWS has divided its programs and activities in five sectoral programs viz.:
+							</p>
+						</div>
+					</div>
+					<div class="lg:flex-grow md:w-1/2 lg:pl-8 md:pl-8 flex flex-col md:items-start md:text-left items-center text-center">
+						<div class="flex flex-wrap w-full text-justify">
+							<p class="w-full leading-relaxed text-gray-500 mb-2">
+								The MSWS has also spelled out its, vision, mission and strategy as well as to complete necessary registrations to be a successful voluntary organization for the upliftment of the poor and needy people of the area.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import Footer from '@/components/footer/footer.vue'
+
+export default {
+	name: 'About-Us',
+	data: () => ({
+	}),
+	components: {
+		Footer
+	},
+	computed: {
+	},
+	methods: {
+	},
+	beforeMount() {
+	},
+	mounted() {
+	}
+}
+</script>
+
+<style scoped></style>
