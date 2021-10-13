e1b234944acd87802efcf50d3f816f1a5a7b14f4
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-13 11:50:48 +0530

    added nuxt config

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
+
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'Mangalik Social Welfare Society',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+		]
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+	],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+	],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: false,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		'@nuxtjs/tailwindcss',
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		'@nuxtjs/axios',
+	],
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {},
+
+	tailwindcss: {
+		viewer: false
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+	}
+}
