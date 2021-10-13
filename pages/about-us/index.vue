fda9d8c58c12189561eb086bd5372be79e6ddc86
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-13 18:58:52 +0530

    updated about us

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -2,44 +2,86 @@
 	<div class="w-full">
 		<section class="text-gray-600 body-font bg-indigo-100">
 			<div class="container px-5 pt-24 pb-10 mx-auto flex flex-wrap items-center">
-				<div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-					<h1 class="title-font font-medium text-3xl text-gray-900">
-						Mangalik Social Welfare Society
-					</h1>
-					<p class="leading-relaxed mt-4 text-justify">
-						The <b>Mangalik Social Welfare Society (MSWS)</b> has completed its second formal years and marching for the third formal years of its existence for extending welfare and development services for the people of North Tripura district.  However, the Society was emerged with another name, <b>Bharatiya Health Mission (BHM)</b> but considering similar names of one central government, the same was declined and was registered as <b>Mangalik Social Welfare Society (MSWS)</b> on <b>6th April 2017</b>. Since inception, the MSWS is extending its welfare and development services for the needy poor people in one hand and also organizing its efforts for its organizational development.
+				<h1 class="title-font font-medium text-3xl text-gray-900">
+					About Us
+				</h1>
+			</div>
+			<div class="container px-5 mx-auto flex flex-wrap items-start">
+				<div class="lg:w-1/2 md:w-1/2 p-3 pt-0">
+					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+						Our Mission
+					</h3>
+					<p class="leading-relaxed text-justify">
+						To organize people for their socio-economic, socio-cultural and for improvement of health and environment with special attention to promote better services for health, education, environment and livelihood for every person by spreading and extending it mainly among the needy poor persons residing in remote areas
 					</p>
-					<div class="flex flex-wrap w-full text-justify">
-						<p class="w-full leading-relaxed text-gray-500">
-							This year it has also taken initiatives to make its community clinic little larger and even the village panchayat Bargul has also extends its financial support for the same.
-						</p>
-					</div>
-
 				</div>
-				<div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
 
+				<div class="lg:w-1/2 md:w-1/2 p-3 pt-0">
+					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+						Our Vision
+					</h3>
+					<p class="leading-relaxed text-justify">
+						To organize specialized services and interventions towards extending planned development services on health, education, environment, livelihood and culture to enable a common people to adopt better life-style for a peaceful and progressive living in the society
+					</p>
+				</div>
+			</div>
+
+			<div class="container px-5 pb-10 mx-auto flex flex-wrap items-start">
+				<div class="lg:w-1/2 md:w-1/2 p-3 pt-0">
+					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+						Strategies Vision
+					</h3>
+					<p class="leading-relaxed text-justify">
+						The operational strategies for development intervention of MSWS are based on the programs and projects intends to uplift the poor, needy, deprived persons and with special priority for the women.   The operation strategies are focusing towards five (5) sectoral programs and activities with special reference to the socio-economic-cultural empowerment of the people especially those are needy, poor, deprived and marginalized persons.
+
+						The MSWS organizes its operations based on local resources, skills and markets and more specifically for the livelihoods programs incorporate with the utilization of traditional and natural resources for helping the needy people to have a regular flow of income round the year.  It also engaged itself to promote community harmony and national integration to share nation building responsibility.
+					</p>
+				</div>
+
+				<div class="lg:w-1/2 md:w-1/2 p-3 pt-0">
+					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+						Legal Status
+					</h3>
+					<p class="leading-relaxed text-justify">
+						To organize specialized services and interventions towards extending planned development services on health, education, environment, livelihood and culture to enable a common people to adopt better life-style for a peaceful and progressive living in the society
+					</p>
 				</div>
 			</div>
 		</section>
 
 		<section class="text-gray-600 body-font py-12">
-			<div class="container px-5 mx-auto flex flex-wrap items-center">
-				<div class="flex flex-wrap w-full text-justify">
-					<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-						<div class="flex flex-wrap w-full text-justify pb-6">
-							<p class="w-full leading-relaxed text-gray-500">
-								The MSWS has adopted suitable strategies for implementation of its development interventions and to make it more transparent, the MSWS has divided its programs and activities in five sectoral programs viz.:
-							</p>
-						</div>
-					</div>
-					<div class="lg:flex-grow md:w-1/2 lg:pl-8 md:pl-8 flex flex-col md:items-start md:text-left items-center text-center">
-						<div class="flex flex-wrap w-full text-justify">
-							<p class="w-full leading-relaxed text-gray-500 mb-2">
-								The MSWS has also spelled out its, vision, mission and strategy as well as to complete necessary registrations to be a successful voluntary organization for the upliftment of the poor and needy people of the area.
-							</p>
-						</div>
-					</div>
-				</div>
+			<div class="container px-5 pb-10 mx-auto">
+					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+						Objectives
+					</h3>
+					<p class="leading-relaxed text-justify my-2 font-medium">
+						Major objectives of Mangalik Social Welfare Society with latest amendments
+					</p>
+					<ol class="list-decimal ml-8">
+						<li>To establish and maintain libraries and reading rooms for the welfare of the common people.</li>
+						<li>To promote agriculture and allied activities and animal resource development for welfare of the society.</li>
+						<li>To generate public awareness about potential of nutrition, homeopathy, bio-chemic, and ayurvedic system of medicine for enhancing health security of rural communities including disease prevention and health promotion.</li>
+						<li>To organize and undertake trainings on village industries for the welfare of the interested persons for self-employment</li>
+						<li>To provide care through ayurvedic, nutrition, homeopathy and bio-chemic system of medicine to suffering humanity</li>
+						<li>To generate public awareness in Tripura under bio-medical waste (management & handling)</li>
+						<li>To confer motivational speech for the mental development and welfare of the prisoners and perverts peoples</li>
+						<li>To ensure girls educational supports and women’s rights and empowerment of their security in both rural and urban slums</li>
+						<li>To fight and welfare against child labour</li>
+						<li>To accept bequeath, gift, grant or endowment for the objects of the society</li>
+						<li>To publish books, journals and posters for betterment of young stars and for the attainment of objectives of the society. Continued from the next page. Continued from the last page</li>
+						<li>To acquire by purchase or by lease or by otherwise, land and building and other properties movable or immovable for the society from time to time may think fit for the welfare of the society</li>
+						<li>To collect subscriptions and donations from members and others to attain the object of the society</li>
+						<li>To work as a non-political organization</li>
+						<li>To do all such other things as may be conducive or incidental for attainment of all or any of the above objectives</li>
+						<li>To organize vocational training courses for promotion of self-employment among the rural poor for sustainable livelihoods</li>
+						<li>To conduct awareness programs on critical disease like: HIV-AIDS, Cancer, Diabetes, Tuberculosis, and Hepatitis for their control and to take up preventive measures</li>
+						<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
+						<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
+						<li>To establish special coaching classes for the disabled, run residential school and hostels for promotion of education among disabled persons</li>
+						<li>To organize capacity building seminars, workshops, health camps, eye camps, blood donation camps, environment and sanitation awareness camps and it can run health centre, ayurvedic hospital to improve the health status of poor people</li>
+						<li>To organize planned actions for latest skill development trainings and to assist in promotion of men and women groups for adoption of cluster approach for sustainable livelihoods</li>
+					</ol>
+				<!-- </div> -->
 			</div>
 		</section>
 
