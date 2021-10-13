b018f2532ef8167535e31d0c9f5c8d041f00d41c
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-13 11:50:36 +0530

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex h-screen relative">
+		<Sidebar class="w-1/6 z-50 fixed"/>
+		<div class="flex-none w-1/6"/>
+		<Nuxt class="flex-grow z-0"/>
+	</div>
+</template>
+
+<script>
+import Footer from '@/components/footer/footer.vue'
+import Sidebar from '@/components/navbar/sidebar.vue'
+
+export default {
+	data: () => ({
+	}),
+	components: {
+		Footer,
+		Sidebar,
+	},
+	computed: {
+	},
+	beforeMount(){
+	},
+};
+</script>
+
+<style scoped></style>
