8614fdd101b86dc59fcfc1c509f3a3bee0f32734
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-15 18:18:10 +0530

    updated for mobile version

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,13 +1,13 @@
 <template>
 	<footer class="text-gray-600 body-font bg-indigo-100">
-		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-			<a class="flex title-font font-bold items-center md:justify-start justify-center text-gray-900">
+		<div class="container px-5 py-8 mx-auto flex items-center flex-col">
+			<a class="flex title-font font-bold items-center justify-center text-gray-900">
 				<span class="ml-3 text-xl">Mangalik Social Welfare Society</span>
 			</a>
-			<p class="text-sm text-gray-500 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+			<p class="text-sm text-gray-500 mt-4">
 				© 2021 MSWS | Developed by <a href="#" rel="noopener noreferrer" target="_blank">Animesh Kumar Singh</a>
 			</p>
-			<span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+			<span class="inline-flex mt-4 justify-center">
 				<a class="text-gray-500">
 					<svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
 						<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
