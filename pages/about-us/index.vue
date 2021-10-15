cf0e246bf0f5cdbf69be0d944c6c7104d1a9f837
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-15 12:48:31 +0530

    minor edit

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -51,37 +51,36 @@
 
 		<section class="text-gray-600 body-font py-12">
 			<div class="container px-5 pb-10 mx-auto">
-					<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
-						Objectives
-					</h3>
-					<p class="leading-relaxed text-justify my-2 font-medium">
-						Major objectives of Mangalik Social Welfare Society with latest amendments
-					</p>
-					<ol class="list-decimal ml-8">
-						<li>To establish and maintain libraries and reading rooms for the welfare of the common people.</li>
-						<li>To promote agriculture and allied activities and animal resource development for welfare of the society.</li>
-						<li>To generate public awareness about potential of nutrition, homeopathy, bio-chemic, and ayurvedic system of medicine for enhancing health security of rural communities including disease prevention and health promotion.</li>
-						<li>To organize and undertake trainings on village industries for the welfare of the interested persons for self-employment</li>
-						<li>To provide care through ayurvedic, nutrition, homeopathy and bio-chemic system of medicine to suffering humanity</li>
-						<li>To generate public awareness in Tripura under bio-medical waste (management & handling)</li>
-						<li>To confer motivational speech for the mental development and welfare of the prisoners and perverts peoples</li>
-						<li>To ensure girls educational supports and women’s rights and empowerment of their security in both rural and urban slums</li>
-						<li>To fight and welfare against child labour</li>
-						<li>To accept bequeath, gift, grant or endowment for the objects of the society</li>
-						<li>To publish books, journals and posters for betterment of young stars and for the attainment of objectives of the society. Continued from the next page. Continued from the last page</li>
-						<li>To acquire by purchase or by lease or by otherwise, land and building and other properties movable or immovable for the society from time to time may think fit for the welfare of the society</li>
-						<li>To collect subscriptions and donations from members and others to attain the object of the society</li>
-						<li>To work as a non-political organization</li>
-						<li>To do all such other things as may be conducive or incidental for attainment of all or any of the above objectives</li>
-						<li>To organize vocational training courses for promotion of self-employment among the rural poor for sustainable livelihoods</li>
-						<li>To conduct awareness programs on critical disease like: HIV-AIDS, Cancer, Diabetes, Tuberculosis, and Hepatitis for their control and to take up preventive measures</li>
-						<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
-						<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
-						<li>To establish special coaching classes for the disabled, run residential school and hostels for promotion of education among disabled persons</li>
-						<li>To organize capacity building seminars, workshops, health camps, eye camps, blood donation camps, environment and sanitation awareness camps and it can run health centre, ayurvedic hospital to improve the health status of poor people</li>
-						<li>To organize planned actions for latest skill development trainings and to assist in promotion of men and women groups for adoption of cluster approach for sustainable livelihoods</li>
-					</ol>
-				<!-- </div> -->
+				<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+					Objectives
+				</h3>
+				<p class="leading-relaxed text-justify my-2 font-medium">
+					Major objectives of Mangalik Social Welfare Society with latest amendments
+				</p>
+				<ol class="list-decimal ml-8">
+					<li>To establish and maintain libraries and reading rooms for the welfare of the common people.</li>
+					<li>To promote agriculture and allied activities and animal resource development for welfare of the society.</li>
+					<li>To generate public awareness about potential of nutrition, homeopathy, bio-chemic, and ayurvedic system of medicine for enhancing health security of rural communities including disease prevention and health promotion.</li>
+					<li>To organize and undertake trainings on village industries for the welfare of the interested persons for self-employment</li>
+					<li>To provide care through ayurvedic, nutrition, homeopathy and bio-chemic system of medicine to suffering humanity</li>
+					<li>To generate public awareness in Tripura under bio-medical waste (management & handling)</li>
+					<li>To confer motivational speech for the mental development and welfare of the prisoners and perverts peoples</li>
+					<li>To ensure girls educational supports and women’s rights and empowerment of their security in both rural and urban slums</li>
+					<li>To fight and welfare against child labour</li>
+					<li>To accept bequeath, gift, grant or endowment for the objects of the society</li>
+					<li>To publish books, journals and posters for betterment of young stars and for the attainment of objectives of the society. Continued from the next page. Continued from the last page</li>
+					<li>To acquire by purchase or by lease or by otherwise, land and building and other properties movable or immovable for the society from time to time may think fit for the welfare of the society</li>
+					<li>To collect subscriptions and donations from members and others to attain the object of the society</li>
+					<li>To work as a non-political organization</li>
+					<li>To do all such other things as may be conducive or incidental for attainment of all or any of the above objectives</li>
+					<li>To organize vocational training courses for promotion of self-employment among the rural poor for sustainable livelihoods</li>
+					<li>To conduct awareness programs on critical disease like: HIV-AIDS, Cancer, Diabetes, Tuberculosis, and Hepatitis for their control and to take up preventive measures</li>
+					<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
+					<li>To assist and cooperate for execution of various welfare and development programs for the state government departments, ministries and sponsoring agencies</li>
+					<li>To establish special coaching classes for the disabled, run residential school and hostels for promotion of education among disabled persons</li>
+					<li>To organize capacity building seminars, workshops, health camps, eye camps, blood donation camps, environment and sanitation awareness camps and it can run health centre, ayurvedic hospital to improve the health status of poor people</li>
+					<li>To organize planned actions for latest skill development trainings and to assist in promotion of men and women groups for adoption of cluster approach for sustainable livelihoods</li>
+				</ol>
 			</div>
 		</section>
 
