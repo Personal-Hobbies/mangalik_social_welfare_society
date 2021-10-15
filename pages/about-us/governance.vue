b6175736edc8325d5af3797c0372ddcb89cdcfc2
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-15 18:32:25 +0530

    added organization chart to governance

--- pages/about-us/governance.vue
+++ pages/about-us/governance.vue
@@ -2,7 +2,7 @@
 	<div class="w-full">
 		<div class="min-h-screen">
 			<section class="text-gray-600 body-font bg-yellow-100">
-				<div class="container px-16 sm:px-5 pt-24 pb-10 mx-auto flex flex-wrap items-center">
+				<div class="container px-16 sm:px-5 pt-24 pb-5 mx-auto flex flex-wrap items-center">
 					<div>
 						<h1 class="title-font font-medium text-3xl text-gray-900">
 							Governance
@@ -30,6 +30,53 @@
 							</p>
 						</div>
 					</div>
+				</div>
+
+				<div class="container px-16 sm:px-5 mx-auto pb-10">
+					<div class="grid grid-cols-1 gap-4">
+						<div class="bg-orange-400 h-18 w-full rounded-md flex items-center justify-center text-white text-xl font-bold p-2 text-center">Mangalik Social Welfare Society (MSWS)</div>
+					</div>
+
+					<div class="flex space-x-4 sm:space-x-0 pt-4 sm:px-1">
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-grow bg-lime-400 h-12 rounded-md flex items-center justify-center text-white text-xl font-bold">General Body</div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+					</div>
+
+					<div class="flex space-x-4 sm:space-x-0 pt-4 sm:px-2">
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-grow bg-green-400 h-12 rounded-md flex items-center justify-center text-white text-xl font-bold">Executive Committee</div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+					</div>
+
+					<div class="flex space-x-4 sm:space-x-0 pt-4 sm:px-4">
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-grow bg-emerald-400 h-12 rounded-md flex items-center justify-center text-white text-xl font-bold">Programmes & Activites</div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+						<div class="flex-none w-16 h-12 rounded-md flex items-center justify-center sm:hidden"></div>
+					</div>
+
+					<div class="grid grid-cols-7 gap-2 pt-4 sm:px-6 sm:grid-cols-1">
+						<div class="h-12 rounded-md flex items-center justify-center invisible sm:hidden"></div>
+						<div class="sm:h-12 h-16 bg-sky-400 rounded-md flex items-center justify-center text-white text-sm font-medium p-2 text-center">Health Education Programmes</div>
+						<div class="sm:h-12 h-16 bg-blue-400 rounded-md flex items-center justify-center text-white text-sm font-medium p-2 text-center">Health Programmes</div>
+						<div class="sm:h-12 h-16 bg-indigo-400 rounded-md flex items-center justify-center text-white text-sm font-medium p-2 text-center">Livelihood Programmes</div>
+						<div class="sm:h-12 h-16 bg-violet-400 rounded-md flex items-center justify-center text-white text-sm font-medium p-2 text-center">Environment Programmes</div>
+						<div class="sm:h-12 h-16 bg-purple-400 rounded-md flex items-center justify-center text-white text-sm font-medium p-2 text-center">Community Developement Programmes</div>
+						<div class="h-12 rounded-md flex items-center justify-center invisible sm:hidden"></div>
+					</div>
+
 				</div>
 			</section>
 
