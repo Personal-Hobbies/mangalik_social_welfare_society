3d11e4bc3997f55936c98c066f9a42a68cf927c0
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-17 20:38:04 +0530

    added health education

---
+++ pages/intervention/health-education.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="w-full">
+		<div class="min-h-screen">
+			<section class="text-gray-600 body-font bg-yellow-100">
+				<div class="container px-16 sm:px-5 pt-24 pb-10 mx-auto flex flex-wrap items-center">
+					<h1 class="title-font font-medium text-3xl text-gray-900">
+						Health & Education
+					</h1>
+				</div>
+
+				<div class="container px-16 sm:px-5 pb-10 mx-auto flex flex-wrap items-start">
+					<div class="w-full p-3 sm:p-0">
+						<p class="leading-relaxed mt-4 text-justify">
+							The legal entity of the MSWS permits is to operate or initiate all its welfare and development activities in the state of Tripura but lack of required development resources, the MSWS is presently organizing its development operations in eight (8) R. D. Blocks of two Sub-Divisions namely:
+						</p>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import Footer from '@/components/footer/footer.vue'
+
+export default {
+	name: 'Health-Education',
+	data: () => ({
+	}),
+	components: {
+		Footer
+	},
+	computed: {
+	},
+	methods: {
+	},
+	beforeMount() {
+	},
+	mounted() {
+	}
+}
+</script>
+
+<style scoped></style>
