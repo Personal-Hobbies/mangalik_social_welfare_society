73c815c6ffdf45918a93274b2b52fe4ed594bdab
Animesh_Singh <animesh.workplace@gmail.com>
2021-10-26 18:50:27 +0530

    added operations

---
+++ pages/operations.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="w-full">
+		<div class="min-h-screen">
+			<section class="text-gray-600 body-font bg-yellow-100">
+				<div class="container px-16 sm:px-5 pt-24 pb-10 mx-auto flex flex-wrap items-center">
+					<h1 class="title-font font-medium text-3xl text-gray-900">
+						Operations
+					</h1>
+				</div>
+
+				<div class="container px-16 sm:px-5 pb-10 mx-auto flex flex-wrap items-start">
+					<div class="w-full p-3 sm:p-0">
+						<p class="leading-relaxed mt-4 text-justify">
+							The operations of Mangalik Social Welfare Society (MSWS) are based on its laid down objectives which was also amended in 2018 and its development interventions are based on a few sectoral programs.  MSWS started its operations with some charity based activities but with the growing needs of people and limited resources has guided to organize its operations in accordance to available resources on priority programs. The resource of MSWS are mainly comprises of subscriptions, donations, contributions and projects grants. MSWS is now approaching various departments and ministries for mobilizing grants-in-aids for its specialized development interventions.
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="text-gray-600 body-font">
+				<div class="container px-16 sm:px-5 mx-auto">
+					<div class="container py-4 mx-auto flex flex-wrap items-start">
+						<div class="w-1/2 sm:w-full p-3 sm:p-0">
+							<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+								Operational Areas
+							</h3>
+							<p class="leading-relaxed text-justify">
+								The Mangalik Social Welfare Society organizes its development operations primarily in the North Tripura district, where it was formed/established.  As per its legal status the Society can organize its welfare and development operations in any parts of the state of Tripura.  The Society has started organizing all its development initiatives from Kadamtala areas and then tried to cover entire panchayat areas and slowly moved to other panchayats and rural development (RD) blocks to cover entire North Tripura district.
+							</p>
+							<p class="leading-relaxed text-justify">
+								Presently, the MSWS organizes its major programs and activities in collaboration with the district administration in two (2) specific Sub-divisions of North Tripura district namely:
+								<ul class="list-disc pl-8">
+									<li>Kadamtala</li>
+									<li>Kanchanpur Sub-divisions covering six (6) RD Blocks</li>
+								</ul>
+								MSWS has also decided to extend its coverage in all the three (3) Sub-divisions and eight (8) RD Blocks of the North district of the state.
+							</p>
+						</div>
+
+						<div class="w-1/2 sm:w-full p-3 sm:p-0">
+							<h3 class="title-font font-medium text-lg text-gray-900 mt-4">
+								Operational Strategy
+							</h3>
+							<p class="leading-relaxed text-justify">
+								The Mangalik Social Welfare Society organizes its welfare and developmental programs using the strategies that can cover the needy poor, deprived persons and persons got special priorities namely: women, orphans, adolescent girls and children.  Considering the needs for welfare and development initiatives, MSWS organizes its strategies to introduce five (5) sectoral programs for socio-economic-cultural empowerment of the targeted people.
+							</p>
+							<p class="leading-relaxed text-justify">
+								MSWS adopted its operational strategies for introducing planned or organized development interventions not only to response to the needs of people but also organize actions to solve their problem at the larger level. To solve their problems, MSWS may use locally available resources, skills trainings for both existing and for acquiring new skills, encourage for under taking planned production and thereby packing and making of quality finished products to get reasonable returns to overcome the causes of poverty and to lead a progressive life-style and have regular flow of income for their sustainable livelihoods. MSWS will also encourage for promotion of national integration and communal harmony.
+
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</div>
+
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import Footer from '@/components/footer/footer.vue'
+
+export default {
+	name: 'Future-Inititatives',
+	data: () => ({
+	}),
+	components: {
+		Footer
+	},
+	computed: {
+	},
+	methods: {
+	},
+	beforeMount() {
+	},
+	mounted() {
+	}
+}
+</script>
+
+<style scoped></style>
